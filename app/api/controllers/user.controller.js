34a12cbd1ef02d92f3332ccdf2d17653529bcb2e
Chhikara-Himanshu <hchikkara@solveda.com>
2022-06-13 23:22:38 +0530

    added handling for add user in db

--- app/api/controllers/user.controller.js
+++ app/api/controllers/user.controller.js
@@ -2,16 +2,19 @@ const userModel = require('../models/users');
 const bcrypt = require('bcrypt'); 
 const jwt = require('jsonwebtoken');
 module.exports = {
- create: function(req, res, next) {
-  
-  userModel.create({ name: req.body.name, email: req.body.email, password: req.body.password }, function (err, result) {
-      if (err) 
-       next(err);
-      else
-       res.json({status: "success", message: "User added successfully!!!", data: null});
-      
-    });
- },
+ create: async function(req, res, next) {
+  let findUser = await userModel.findOne({email: req.body.email})
+  if(findUser) {
+    return res.status(400).json({status: "Error", message: "User allready exists", data: null}); 
+  }
+    let result = await userModel.create({ name: req.body.name, email: req.body.email, password: req.body.password });
+    if(result) {
+        return res.status(200).json({status: "success", message: "User added successfully!!!", data: null});
+    }
+    else {
+        return res.status(400).json({status: "Error", message: "Something wrong happened", data: null});  
+    }
+},
 authenticate: function(req, res, next) {
     userModel.findOne({email:req.body.email}, function(err, userInfo){
     if (err) {
